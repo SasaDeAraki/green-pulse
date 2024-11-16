8ddad721453b3036f52728856c019358a7f6427f
otaviouzal <uzalotavio@gmail.com>
2024-11-16 13:56:30 -0300

    alterando algumas coisas no Header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 export default function Footer() {
     return (
       <footer style={{ backgroundColor: "#333", padding: "10px", color: "white", textAlign: "center", marginTop: "20px" }}>
-        <p>© 2024 Meu Site. Todos os direitos reservados.</p>
+        <p>© 2024</p>
+        <p>GreenPulse</p>
       </footer>
     );
   }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,13 @@ import './Header.css';
 
 export default function Header() {
     return (
-      <header style={{ backgroundColor: "#98ff96", padding: "10px", color: "white", textAlign: "center" }}>
+      <header>
         <nav>
-            <h1><a href="/">GreenPulse</a></h1>
+            <h1>GreenPulse</h1>
             <ul>
-                <li><a href="/Tutorial">Tutorial</a></li>
-                <li><a href="/about">Sobre</a></li>
-                <li><a href="/faq">FAQ</a></li>
+                <li>Tutorial</li>
+                <li>Sobre</li>
+                <li>FAQ</li>
             </ul>
         </nav>
       </header>
