21b7fb56b37b3a715faa60120596f08637d792b4
unknown <uzalotavio@gmail.com>
2024-11-19 10:53:56 -0300

    Pag Prduto base pronta

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer style={{ backgroundColor: "#333", padding: "10px", color: "white", textAlign: "center", marginTop: "20px" }}>
+      <footer style={{ backgroundColor: "#789756", padding: "10px", color: "white", textAlign: "center", marginTop: "20px" }}>
         <p>GreenPulse</p>
         <p>©2024</p>
       </footer>
