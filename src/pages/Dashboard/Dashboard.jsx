155939f19d464844726d14172cf9f33802b6e301
unknown <uzalotavio@gmail.com>
2024-11-19 15:30:23 -0300

    dashboard teste

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,40 @@
+import React, { useState } from "react";
+
+function Dashboard1() {
+    return <h1>Dashboard 1</h1>;
+}
+function Dashboard2() {
+    return <h1>Dashboard 2</h1>;
+}
+function Dashboard3() {
+    return <h1>Dashboard 3</h1>;
+}
+
 export default function Dashboard() {
-    return(
-        <>
-            <div style={{ marginBottom: 110, marginTop: 80}}>
-                <h1 style={{color: 'black', fontSize: '25px'}}>Dashboard</h1>
+    const [activeDashboard, setActiveDashboard] = useState("Dashboard1");
+
+    const dashboards = {
+        Dashboard1: <Dashboard1 />,
+        Dashboard2: <Dashboard2 />,
+        Dashboard3: <Dashboard3 />,
+    };
+
+    return (
+        <div style={{ marginBottom: 110, marginTop: 80 }}>
+            <div className="buttons">
+                <button onClick={() => setActiveDashboard("Dashboard1")}>
+                    Mostrar Dashboard 1
+                </button>
+                <button onClick={() => setActiveDashboard("Dashboard2")}>
+                    Mostrar Dashboard 2
+                </button>
+                <button onClick={() => setActiveDashboard("Dashboard3")}>
+                    Mostrar Dashboard 3
+                </button>
             </div>
-        </>
-    )
+            <div style={{ marginTop: 20 }}>
+                {dashboards[activeDashboard]}
+            </div>
+        </div>
+    );
 }
