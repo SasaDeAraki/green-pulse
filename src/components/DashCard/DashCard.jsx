0a7dc2860998d84fa838e9272d851ae8951dc61b
Samuel Batista Viana <samuel.viana.yl@gmail.com>
2024-11-21 01:46:44 -0300

    Finalização da página estatísticas

--- src/components/DashCard/DashCard.jsx
+++ src/components/DashCard/DashCard.jsx
@@ -1,15 +1,15 @@
 import './DashCard.css'
 
-export default function DashCard() {
+export default function DashCard({titulo, sub, content}) {
     return(
         <>  
             <div className='dashcard'>
                 <div className='texto'>
-                    <h3>Título</h3>
-                    <p>sub-título</p>
+                    <h3>{titulo}</h3>
+                    <p>{sub}</p>
                 </div>
                 <div className='valor'>
-                    <h1>300K</h1>
+                    <h1>{content}</h1>
                 </div>
             </div>
         </>
